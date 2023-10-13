e7a1a23cdbe7a67d08004d73ba29b93e9c68c006
chenyouhui1 <chenyouhui1@joyy.com>
2023-10-13 10:30:55 +0800

    构建运行脚本

---
+++ FirstDemo/run.bat
@@ -0,0 +1,30 @@
+
+@REM release or debug
+if /I "%1"=="d" goto DebugMode
+if /I "%1"=="debug" goto DebugMode
+if /I "%1"=="Debug" goto DebugMode
+if /I "%1"=="DEBUG" goto DebugMode
+
+if /I "%1"=="r" goto ReleaseMode
+if /I "%1"=="release" goto ReleaseMode
+if /I "%1"=="Release" goto ReleaseMode
+if /I "%1"=="RELEASE" goto ReleaseMode
+
+else goto DebugMode
+
+:DebugMode
+    set conf=Debug
+    goto End
+:ReleaseMode
+    set conf=Release
+    goto End
+:End
+
+cmake -S . -B cmake-build -G "Visual Studio 17 2022" -A "Win32"
+cmake --build cmake-build --config %conf%
+
+@REM get folder name
+for /f "delims=" %%i in ("%cd%") do set folder=%%~ni
+
+@REM run exe
+.\cmake-build\bin\%folder%\%conf%\%folder%.exe
